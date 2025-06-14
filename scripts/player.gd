d5f6044147f0bad2d6c8d2127cb74f03f6a24887
caira <gontiniascaira@gmail.com>
2025-06-15 00:34:37 +0800

    feat: added animation for walking

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,7 +17,20 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	#Flip the sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = true
+	elif direction < 0:
+		animated_sprite_2d.flip_h = false
+		
+	#Play animations
+	if direction == 0:
+		animated_sprite_2d.play("idle")
+	else:
+		animated_sprite_2d.play("run")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
