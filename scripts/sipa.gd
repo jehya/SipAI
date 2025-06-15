34c31b13bc0dcb24dc91015a8b8e2afa313a7afa
caira <gontiniascaira@gmail.com>
2025-06-15 12:57:24 +0800

    feat: initial power meter

--- scripts/sipa.gd
+++ scripts/sipa.gd
@@ -3,6 +3,8 @@ extends RigidBody2D
 var player_touching = false
 
 @onready var area_2d: Area2D = $Area2D
+@onready var power_meter: Control = $"../CanvasLayer/PowerMeter"
+@onready var player: CharacterBody2D = $"../Player"
 
 func _ready():
 	$Area2D.body_entered.connect(_on_body_entered)
@@ -18,4 +20,17 @@ func _on_body_exited(body):
 
 func _physics_process(delta):
 	if player_touching and Input.is_action_just_pressed("dribble"):
-		apply_central_impulse(Vector2(0, -400))  # Jump upward
+		var power_ratio = power_meter.get_power_ratio()
+		var force = lerp(300, 800, power_ratio)
+
+		var horizontal = 0
+		if Input.is_action_pressed("move_right"):
+			horizontal += 1
+		if Input.is_action_pressed("move_left"):
+			horizontal -= 1
+
+		# Compute the impulse based on direction
+		var impulse = Vector2(horizontal * 200, -force).normalized() * force
+		apply_central_impulse(impulse)
+
+		print("Impulse: ", impulse)
